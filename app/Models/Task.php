74b2e1cea726959dc9165fb021d671a41184a0be
rasyid annas <rasshit.dsgn@gmail.com>
2024-02-27 13:38:00 +0700

    2/3. Model Resources.mp4

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    //this will change is_done num value (0 or 1) to be false/true in json
+    protected $casts = [
+        'is_done' => "boolean"
+    ];
+
+    protected $hidden = [
+        "updated_at"
+    ];
 }
